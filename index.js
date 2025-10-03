41aee5b9c5e37935e9ed0907c370e4dbd99229b2
vishal singh tomar <vishaltomar731@gmail.com>
2025-10-03 18:11:02 +0530

    updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
 const app = express();
